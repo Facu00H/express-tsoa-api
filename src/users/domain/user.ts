fc9d70f67e8371377bf3d1d446f889f372f2251b
Facu00H <faisher00@gmail.com>
2023-04-14 19:39:05 -0300

    Create User Class

--- src/users/domain/user.ts
+++ src/users/domain/user.ts
@@ -1,14 +1,15 @@
 import { IUser } from "./interface/IUser";
+import { v4 as uuidv4 } from 'uuid';
 
 export class User implements IUser{
-  id: number;
+  id: string;
   email: string;
   name: string;
   status?: "Happy" | "Sad";
   phoneNumbers: string[];
 
-  constructor(id: number, email: string, name: string, phoneNumbers: string[], status?: "Happy" | "Sad") {
-    this.id = id;
+  constructor(email: string, name: string, phoneNumbers: string[], status?: "Happy" | "Sad") {
+    this.id = uuidv4();
     this.email = email;
     this.name = name;
     this.status = status;
