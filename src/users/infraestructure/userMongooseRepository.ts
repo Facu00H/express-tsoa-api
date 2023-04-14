556565a451c320f0238af7c31491ab191db53ca3
Facu00H <faisher00@gmail.com>
2023-04-14 19:37:19 -0300

    create methods for UserRepository using mongodb

---
+++ src/users/infraestructure/userMongooseRepository.ts
@@ -0,0 +1,26 @@
+import { IUserRepository } from "../domain/interface/IUser";
+import { User } from "../domain/User";
+import { UserModel } from "../../mongo/schemas/UserModel";
+
+
+export class UserRepositoryMongo implements IUserRepository {
+  async create(user: User): Promise<User> {
+    const userCollection = await UserModel.findOne({ name: user.name });
+    if (userCollection) {
+      throw new Error("User already exists");
+    }
+
+    const newUser = new UserModel(user);
+    await newUser.save();
+    return newUser;
+  };
+
+  async get(id: string): Promise<User> {
+    const userCollection = await UserModel.findById(id);
+    if (!userCollection) {
+      throw new Error("User not found");
+    }
+
+    return userCollection;
+  }
+}
