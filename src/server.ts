6d8a549050e474251933cd05003dc1055ae3dccf
Facu00H <faisher00@gmail.com>
2023-04-14 17:00:25 -0300

    add mongoDb connection func

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import { app } from "./app";
+import connectDb from "./mongo/dbConnection";
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
+
+connectDb()
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.log(error.message));
