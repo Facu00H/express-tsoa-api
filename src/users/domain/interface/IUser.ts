5a91f27cca18ef54e22b5baf45d9e80169472c76
Facu00H <faisher00@gmail.com>
2023-04-18 11:30:17 -0300

    id change to readonly, create IEditUser type, add update method to IUserRepositopry interface

--- src/users/domain/interface/IUser.ts
+++ src/users/domain/interface/IUser.ts
@@ -1,15 +1,18 @@
 import { User } from '../User'
 
 export interface IUser {
-  id: string;
+  readonly id: string;
   email: string;
   name: string;
   status?: "Happy" | "Sad";
   phoneNumbers: string[];
 };
 
+export type IEditUser = Omit<Partial<IUser>, 'id'>
+
 export interface IUserRepository {
   get(id: string): Promise<User>;
   create(user: User): Promise<User>;
   getAll(): Promise<User[]>;
+  update(id: string, user: IEditUser): Promise<User>;
 }
